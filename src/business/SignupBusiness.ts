85d30b00c5445ae7cdd25709e878650f8321ae64
Hugo <hugolacerdar@gmail.com>
2020-08-30 18:55:02 -0300

    Adding Signup Business file

---
+++ src/business/SignupBusiness.ts
@@ -0,0 +1,35 @@
+import UserDB from "../data/UserDB";
+import User, { UserRole } from "../model/User";
+import Cypher from "../services/Cypher";
+import IdGenerator from "../services/IdGenerator";
+export default class SignupBusiness {
+    constructor(
+        private userDB: UserDB,
+        private cypher: Cypher,
+        private idGenerator: IdGenerator
+    ){}
+
+    public async execute(input: SignupBusinessInput): Promise<User> {
+        const hashPassword = await this.cypher.hash(input.password);
+
+        const user = User.toUser({
+            ...input,
+            id: this.idGenerator.generate(),
+            password: hashPassword
+        });
+
+        await this.userDB.createUser(user!);
+
+        return user!;
+    }
+}
+
+export interface SignupBusinessInput {
+    name: string,
+    email: string,
+    username: string, 
+    password: string,
+    role: UserRole,
+    isApproved: boolean,
+    description?: string
+}
