4d16bb11dd8db6b2fbdbcb81972cd8dd01c18435
Hugo <hugolacerdar@gmail.com>
2020-08-31 22:43:01 -0300

    Adding signin business

---
+++ src/business/SigninBusiness.ts
@@ -0,0 +1,38 @@
+import UserDB from "../data/UserDB";
+import User, { UserRole } from "../model/User";
+import Cypher from "../services/Cypher";
+import IdGenerator from "../services/IdGenerator";
+import NotFoundError from "../error/NotFoundError";
+export default class SigninBusiness {
+    constructor(
+        private userDB: UserDB,
+        private cypher: Cypher
+    ) { }
+
+    public async execute(input: SigninBusinessInput): Promise<User> {
+
+        const user = await this.userDB.getUserByEmailOrUsername(input.credential);
+
+        console.log(user);
+        
+        if (!user) {
+            throw new NotFoundError("Invalid credentials");
+        }
+
+        const isPasswordRight = await this.cypher.compare(
+            input.password!,
+            user.getPassword()
+        );
+
+        if (!isPasswordRight) {
+            throw new NotFoundError("Invalid credentials")
+        }
+
+        return user;
+    }
+}
+
+export interface SigninBusinessInput {
+    credential: string,
+    password: string
+}
