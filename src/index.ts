b50c8245f5ab2baf0fa1f58e93c851598851b342
Hugo <hugolacerdar@gmail.com>
2020-08-31 22:39:48 -0300

    Fixing local.ts and index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('testando')
+import express from "express";
+import userRouter from "./router/user";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+
+app.use("/users", userRouter);
+
+export default app;
---
+++ src/local.ts
@@ -0,0 +1,16 @@
+import app from "./index";
+import { AddressInfo } from "net";
+import UserDB from "./data/UserDB";
+import BaseDB from "./data/base/BaseDB";
+
+const server = app.listen(process.env.PORT || 3007, () => {
+  if (server) {
+    console.log(
+      `Server listening on http://localhost:${
+        (server.address() as AddressInfo).port
+      }`
+    );
+  } else {
+    console.log(`Error on running server`);
+  }
+});
