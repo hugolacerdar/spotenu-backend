6d2a28a0329ae2c2451907b9b41b55018d16373e
Hugo <hugolacerdar@gmail.com>
2020-09-02 19:10:53 -0300

    Fixing user router

--- src/router/user.ts
+++ src/router/user.ts
@@ -5,5 +5,7 @@ const userRouter = Router();
 
 userRouter.post("/signup", new UserController().signup);
 userRouter.post("/signin", new UserController().signin);
+userRouter.get("/bands", new UserController().getBands);
+userRouter.put("/approve_band", new UserController().approveBand);
 
 export default userRouter;
