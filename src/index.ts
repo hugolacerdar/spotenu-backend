8f6880b23065e831e276f8c4ad81c51c446c9460
Hugo <hugolacerdar@gmail.com>
2020-09-03 15:22:44 -0300

    Adding the music genre router to the app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "./router/user";
 import dotenv from "dotenv";
+import musicGenreRouter from "./router/musicGenre";
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json())
 
 app.use("/users", userRouter);
+app.use("/genres", musicGenreRouter);
 
 export default app;
